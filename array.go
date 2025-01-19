9ec0a19f20b6ed0b4b57f7a150a00e81c433f6b6
Kerim Zunic <mail@kerim.id>
2025-01-19 22:02:16 +0800

    Implement len() function for dynamic array

--- array.go
+++ array.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 type DynamicArray struct {
-	Capacity int
+	data     *[10]int
+	capacity int
+	size     int
 }
 
-func NewDynamicArray(capacity int) *DynamicArray {
-    return &DynamicArray{
-        Capacity:  capacity,
-    }
+func NewDynamicArray() *DynamicArray {
+	return &DynamicArray{
+		data:     new([10]int),
+		capacity: 0,
+		size:     0,
+	}
 }
 
-func main () {
-	testArr := NewDynamicArray(2)
-	fmt.Print(testArr)
+func (arr *DynamicArray) len() int {
+	return arr.capacity
+}
+
+func main() {
+	testArr := NewDynamicArray()
+	testArr2 := NewDynamicArray()
+	fmt.Println("Capacity 1 is: ", (*testArr).len())
+	fmt.Println("Capacity 2 is: ", testArr2.len())
+	fmt.Println("Capacity 1 is: ", testArr.len())
 }
