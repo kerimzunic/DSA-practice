f2584248f155bfb7a528a331a083318afe2ea808
Kerim Zunic <mail@kerim.id>
2025-01-20 14:46:51 +0800

    Add test for NewDynamicArray function

--- dynamicarray/dynamicarray.go
+++ dynamicarray/dynamicarray.go
@@ -9,11 +9,39 @@ type DynamicArray struct {
 func NewDynamicArray() *DynamicArray {
 	return &DynamicArray{
 		data:     new([10]int),
+		capacity: 10,
+		size:     0,
+	}
+}
+
+func NewDynamicArrayFromCapacity(capacity int) *DynamicArray {
+	return &DynamicArray{
+		data:     new([10]int),
+		capacity: capacity,
+		size:     0,
+	}
+}
+
+func NewDynamicArrayFromStaticArray(static_array []int) *DynamicArray {
+	return &DynamicArray{
+		data:     new([10]int),
 		capacity: 0,
 		size:     0,
 	}
 }
 
+func (arr *DynamicArray) resize(elements int) int {
+	return arr.size
+}
+
 func (arr *DynamicArray) Size() int {
+	return arr.size
+}
+
+func (arr *DynamicArray) Add() int {
+	return arr.size
+}
+
+func (arr *DynamicArray) Remove() int {
 	return arr.size
 }
--- dynamicarray/dynamicarray_test.go
+++ dynamicarray/dynamicarray_test.go
@@ -2,3 +2,9 @@ package dynamicarray
 
 import "testing"
 
+func TestNewDynamicArray(t *testing.T) {
+	dynamicArray := NewDynamicArray()
+	if dynamicArray.Size() != 0 {
+		t.Error("Initial size should be 10")
+	}
+}
